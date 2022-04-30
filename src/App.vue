3fff8cb84a4e59fba2d3f2c431141f681fa3cd6d
Loic Chazeau <loic.chazeau@epitech.digital>
2022-04-30 20:22:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="w-40 h-40 bg-pink-700"></div>
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
